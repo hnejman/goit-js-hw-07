a12a8ee6f058effa586e4680dda4c4e6bdb2fa9a
hnejman <108231655+hnejman@users.noreply.github.com>
2023-01-02 13:29:16 +0100

    update

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,22 +3,45 @@ import { galleryItems } from './gallery-items.js';
 
 const output = document.querySelector(".gallery");
 
-output.addEventListener("click", e =>{
-    console.log(galleryItems[e.target.dataset.number].original);
-    basicLightbox.create(`
-		<img width="1400" height="900" src="https://placehold.it/1400x900">
-	`).show()
-})
-
 for(let i = 0; i < 9; i++){
-    const wrapper = document.createElement("div");
-    wrapper.classList.add("gallery__wrapper");
+    const item = document.createElement("div");
+    item.classList.add("gallery__item");
+    output.insertAdjacentElement("beforeend", item);
+
+    const a = document.createElement("a");
+    a.classList.add("gallery__image");
+    a.href = galleryItems[i].original;
+    a.classList.add("min");
+    output.lastChild.insertAdjacentElement("afterbegin", a);
+
     const image = document.createElement("img");
     image.classList.add("gallery__image");
+    image.alt=galleryItems[i].description;
     image.src=galleryItems[i].preview;
-    image.dataset.number = i;
-    output.insertAdjacentElement("beforeend", wrapper);
-    output.lastChild.insertAdjacentElement("afterbegin", image);
-}
+    image.dataset.source = galleryItems[i].original;
+    output.lastChild.firstChild.insertAdjacentElement("afterbegin", image);
+};
+
+output.addEventListener("click", e =>{
+    const instance = basicLightbox.create(`
+    <img src="" alt="" loading="lazy" width="800" height="600">
+`)
+instance.show()
+
+    const selector = document.querySelector(".basicLightbox__placeholder");
+    console.log(selector);
+    selector.lastChild.src = e.target.dataset.source;
+    selector.lastChild.alt = e.target.alt
+
+    selector.addEventListener("keydown", (event) => {
+        if (event.isComposing || event.keyCode === 27) {
+            instance.close();
+            console.log("closing");
+        }
+        
+      });
+}, false);
+
+
 
 console.log(galleryItems);
