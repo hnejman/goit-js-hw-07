c077fe77ba6c1bff334c803c419eb01f786985bf
hnejman <108231655+hnejman@users.noreply.github.com>
2023-01-03 19:31:03 +0100

    ...

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,30 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const output = document.querySelector(".gallery");
+
+for(let i = 0; i < 9; i++){
+    const a = document.createElement("a");
+    a.classList.add("gallery__link");
+    a.href = galleryItems[i].original;
+    output.insertAdjacentElement("afterbegin", a);
+
+    const image = document.createElement("img");
+    image.classList.add("gallery__image");
+    image.alt=galleryItems[i].description;
+    image.src=galleryItems[i].preview;
+    image.dataset.source = galleryItems[i].original;
+    output.firstChild.insertAdjacentElement("afterbegin", image);
+};
+
+const lightbox = new SimpleLightbox(".gallery .gallery__link");
+
+lightbox.on('shown.simplelightbox', function () {
+	const p = document.createElement("p");
+    let number = document.querySelector(".sl-current");
+    p.textContent = galleryItems[number.textContent].description;
+    const spot = document.querySelector(".sl-wrapper");
+    spot.insertAdjacentElement("beforeend", p);
+});
+
 console.log(galleryItems);
